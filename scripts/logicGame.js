1f02b9671202a1b05d394c8d161e8d5a16f067de
mojtaba ameli bidar <mojtabaas95@gmail.com>
2022-04-18 12:37:57 +0430

    changing logicGame.js so that array Game update

--- scripts/logicGame.js
+++ scripts/logicGame.js
@@ -26,6 +26,9 @@ replayGameBtn.addEventListener('click', replayeBtnClickHandler);
 
 let gameArray = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
 
+// const diagnoseWinner = () => {
+//     if(gameArray)
+// }
 
 const boxClickedHandler = event => {
     // Update active user btn in header app
@@ -73,78 +76,9 @@ const boxClickedHandler = event => {
         default:
             break;
     }
-    console.log(gameArray);
-    console.log(btnClickedId);
+    event.currentTarget.removeEventListener('click', boxClickedHandler);
 };
 
 for (const box of gameBoxs) {
     box.addEventListener('click', boxClickedHandler);
 }
-
-
-
-
-
-
-
-// let user2;
-
-// if (user1 === '*') {
-//     user2 = '+';
-// } else if(user1 === '+') {
-//     user2 = '*';
-// }
-
-// const gameBoxs = document.querySelectorAll('.game-box-item');
-
-// let arrayBoxId = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-// const createRandomId = () => {
-//     return (Math.floor((Math.random() * 9)) + 1);
-// }
-
-// const clickBoxCpuUserHandler = () => {
-
-//     let randomId = createRandomId();
-//     while(!arrayBoxId.includes(randomId)) {
-//         randomId = createRandomId();
-//     }
-    
-//     const index = arrayBoxId.indexOf(randomId);
-//     if (index > -1) {
-//         arrayBoxId.splice(index, 1);
-//     }
-
-//     const boxCpuUser = document.getElementById(`box${randomId}`);
-
-//     setTimeout(() => {
-//         if(user2 === '*') {
-//             boxCpuUser.innerHTML = '*';
-//         } else if(user2 === '+') {
-//             boxCpuUser.innerHTML = '+';
-//         }
-//         console.log(randomId);
-//     }, 1500);
-// }
-
-// const clickBoxOneUserHandler = event => {
-//     let boxId = event.target.id;
-//     let numberedId = +boxId.charAt(boxId.length - 1);
-
-//     const index = arrayBoxId.indexOf(numberedId);
-//     if (index > -1) {
-//         arrayBoxId.splice(index, 1);
-//     }
-
-//     if(user1 === '*') {
-//         event.target.innerHTML = '*';
-//     } else if(user1 === '+') {
-//         event.target.innerHTML = '+';
-//     }
-//     clickBoxCpuUserHandler();
-// };
-
-// for (const box of gameBoxs) {
-//     box.addEventListener('click', clickBoxOneUserHandler);
-// }
-
