7d4fc35126621686da56ad6251ca77cc3581e362
mojtaba ameli bidar <mojtabaas95@gmail.com>
2022-04-20 11:52:22 +0430

    i corrected game html file

--- scripts/logicGame.js
+++ scripts/logicGame.js
@@ -28,27 +28,103 @@ let gameArray = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']];
 
 let numberPicked = 0;
 
-const checkWinnerAllRows = (chr) => {
+const checkWinnerAllRows = chr => {
     for (let i = 0; i < 3; i++) {
+        let num = 0;
         for (let j = 0; j < 3; j++) {
+            num++;
             if(gameArray[i][j] !== chr) {
                 break;
             }
         }
-        return i;
+        if(num === 3) {
+            return i;  //number row that is winn.
+        }
     }
     return -1; // show winner not exist in i row
 };
+
+const checkWinnerAllCols = chr => {
+    for(let j = 0; j < 3; j++) {
+        let num = 0;
+        for(let i = 0; i < 3; i++) {
+            num++;
+            if(gameArray[i][j] !== chr) {
+                break;
+            }
+        }
+        if(num === 3) {
+            return j;  //number column that is win.
+        }
+    }
+    return -1;
+};
+
+const checkWinnerAllDiagnal = chr => {
+    // for (let i = 0; i < 3; i++) {
+    //     let num = 0;
+    //     for(let j = 0; j < 3; j++) {
+    //         num++;
+    //         if(gameArray[i][j] !== chr || i !== j) {
+    //             break;
+    //         }
+    //     }
+    //     if(num === 3) {
+    //         return 'diagnal';
+    //     }
+    // }
+    // return -1;
+    let numDiagnol = 0;
+    for(let i = 0; i < 3; i++) {
+        numDiagnol++;
+        if(gameArray[i][i] !== chr) {
+            break;
+        }
+    }
+    
+    let numAntiDiagnol = 0;
+    for(let i = 2; i >= 0; i--) {
+        numAntiDiagnol++;
+        if(gameArray[2 - i][i] !== chr) {
+            break;
+        }
+    }
+
+    if(numDiagnol === 3) {
+        return 'Diagonal row';
+    } else if(numAntiDiagnol === 3) {
+        return 'AntiDiagnol row';
+    } else {
+        return -1;
+    }
+};
+
+const showWinnerBoxUi = (winlocate) => {
+    
+}
 
 const pickWinnerUser = (markType) => {
     if(numberPicked < 5) {
         return;
     }
     
-    let rowWinner = checkWinnerAllRows(markType);
+    let rowWinner = checkWinnerAllRows(markType);  // attention that row start from 0
     if(rowWinner !== -1) {
-        console.log(rowWinner);
+        // showUiWinnerBox(rowWinner);
+        console.log('roe', rowWinner);
     }
+
+    let colWinner = checkWinnerAllCols(markType);  // attention that col start from 0
+    if(colWinner !== -1) {
+        console.log('column', colWinner);
+    }
+
+    let diagnalWin = checkWinnerAllDiagnal(markType);
+    console.log(diagnalWin);
+    // if(diagnalWin !== -1) {
+    //     console.log('win is diagnol row');
+    // }
+
 };
 
 const boxClickedHandler = event => {
