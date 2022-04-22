d3b27fa90243821d1911de4f484c770ff2a59983
mojtaba ameli bidar <mojtabaas95@gmail.com>
2022-04-22 16:31:45 +0430

    finished project in single page html and dynamically inport css and script file

--- scripts/configGame.js
+++ scripts/configGame.js
@@ -4,6 +4,7 @@ localStorage.clear();
 let player1Mark;
 let pickePlayerType;
 
+const container = document.querySelector('.container');
 const xMarkButton = document.querySelector('.x-shape');
 const oMarkButton = document.querySelector('.o-shape');
 
@@ -28,7 +29,7 @@ const oMarkButtonClickHandler = () => {
 xMarkButton.addEventListener('click', xMarkButtonClickHandler);
 oMarkButton.addEventListener('click', oMarkButtonClickHandler);
 
-
+// pick user rival for gaming with you (these two below const is for when i want to mount cpu player)!AFTER!
 const cpuRivalButton = document.querySelector('.pick-rival :first-child');
 const userRivalButton = document.querySelector('.pick-rival :last-child');
 
@@ -38,6 +39,72 @@ userRivalButton.addEventListener('click', event => {
     localStorage.setItem('pickeRivalType', pickePlayerType);
     console.log('clicked');
     if(player1Mark) {
-        location.href = location.origin + '/pages/game.html';
+        container.innerHTML = gamePageHTML;
+        pageStyleSheet.href = './styles/gameStyle.css';
+        // dynamically import logicGame.js script
+        import('./logicGame.js')
+            .then(module => {
+                module.loginGameFunction();
+            });
     }
 });
+
+
+//  select variable of styleSheet and container div html, for changing these; when user pick start game.
+const pageStyleSheet = document.head.querySelector('link:nth-of-type(1)');
+const gamePageHTML = `
+    <div class="app">
+        <div class="header-app">
+            <div class="shaped">
+                <i class="fa fa-close"></i>
+                <i class="fa fa-dot-circle-o"></i>
+            </div>
+            <button class="active-user">
+                <i class="fa fa-close"></i> Turn
+            </button>
+            <button class="replay-game">
+                <i class="fa fa-repeat"></i>
+            </button>
+        </div>
+        <div class="restart-backdrop">
+
+        </div>
+        <div class="restart-modal">
+            <p>RESTART GAME?</p>
+            <button>Cancel</button>
+            <button>Restart</button>
+        </div>
+        <div class="game-box">
+            <div class="game-box-item" id="00"></div>
+            <div class="game-box-item" id="01"></div>
+            <div class="game-box-item" id="02"></div>
+            <div class="game-box-item" id="10"></div>
+            <div class="game-box-item" id="11"></div>
+            <div class="game-box-item" id="12"></div>
+            <div class="game-box-item" id="20"></div>
+            <div class="game-box-item" id="21"></div>
+            <div class="game-box-item" id="22"></div>
+        </div>
+        <div class="score-game">
+            <button>
+                <i class="fa fa-close"></i>
+                <p>0</p>
+            </button>
+            <button>
+                Ties
+                <p>0</p>
+            </button>
+            <button>
+                <i class="fa fa-dot-circle-o"></i>
+                <p>0</p>
+            </button>
+        </div>
+    </div>
+    <!-- backdrop and modal for show winner to user -->
+    <div class="backdrop"></div>
+    <div class="modal-winner">
+    <p><span>x</span> TAKES THE ROUND</p>
+    <button>Quit</button>
+    <button>Next Round</button>
+    </div>
+`;
--- scripts/logicGame.js
+++ scripts/logicGame.js
@@ -1,244 +1,249 @@
-const user1 = localStorage.getItem('player1Mark');
-let user2;
+export const loginGameFunction = () => {
 
-if(user1 === 'x') {
-    user2 = 'o';    
-} else if(user1 === 'o') {
-    user2 = 'x';
-}
+    const user1 = localStorage.getItem('player1Mark');
+    let user2;
 
-const player2Type = localStorage.getItem('pickeRivalType');
+    if(user1 === 'x') {
+        user2 = 'o';    
+    } else if(user1 === 'o') {
+        user2 = 'x';
+    }
 
-const gameBoxs = document.querySelectorAll('.game-box-item');
-const activeUserBtn = document.querySelector('.active-user');
-let activeUser = true; // if activeUser variable is true, therefore x user is active.
+    const player2Type = localStorage.getItem('pickeRivalType');
 
-// replay from first that user's pick X or O mark so that two user soccer is zero.
-const replayGameBtn = document.querySelector('.replay-game');
+    const gameBoxs = document.querySelectorAll('.game-box-item');
+    const activeUserBtn = document.querySelector('.active-user');
+    let activeUser = true; // if activeUser variable is true, therefore x user is active.
 
-//  Restart Button Handler
-const restartModal = document.querySelector('.restart-modal');
-const restartBackdrop = document.querySelector('.restart-backdrop');
-const cancelRestartBtn = restartModal.querySelector('button:nth-of-type(1)');
-const doneRestartBtn = restartModal.querySelector('button:nth-of-type(2)');
+    // replay from first that user's pick X or O mark so that two user soccer is zero.
+    const replayGameBtn = document.querySelector('.replay-game');
 
-const replayeBtnClickHandler = () => {
-    restartModal.style.display = 'block';
-    restartBackdrop.style.display = 'block';
-};
+    //  Restart Button Handler
+    const restartModal = document.querySelector('.restart-modal');
+    const restartBackdrop = document.querySelector('.restart-backdrop');
+    const cancelRestartBtn = restartModal.querySelector('button:nth-of-type(1)');
+    const doneRestartBtn = restartModal.querySelector('button:nth-of-type(2)');
 
-const cancelRestartBtnHanler = () => {
-    restartModal.style.display = 'none';
-    restartBackdrop.style.display = 'none';
-};
+    const replayeBtnClickHandler = () => {
+        restartModal.style.display = 'block';
+        restartBackdrop.style.display = 'block';
+    };
 
-const doneRestartBtnHanler = () => {
-    history.back();
-};
+    const cancelRestartBtnHanler = () => {
+        restartModal.style.display = 'none';
+        restartBackdrop.style.display = 'none';
+    };
 
-cancelRestartBtn.addEventListener('click', cancelRestartBtnHanler);
-doneRestartBtn.addEventListener('click', doneRestartBtnHanler);
+    const doneRestartBtnHanler = () => {
+        restartModal.style.display = 'none';
+        restartBackdrop.style.display = 'none';
+        document.location.reload(true);
+    };
 
-replayGameBtn.addEventListener('click', replayeBtnClickHandler);
+    cancelRestartBtn.addEventListener('click', cancelRestartBtnHanler);
+    doneRestartBtn.addEventListener('click', doneRestartBtnHanler);
 
-let gameArray = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']];
+    replayGameBtn.addEventListener('click', replayeBtnClickHandler);
 
-let numberPicked = 0;  // sum of numberinf of picked of two user.
+    let gameArray = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']];
 
-const checkWinnerAllRows = chr => {
-    for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-            if(gameArray[i][j] !== chr) {
-                break;
-            }
-            if(j === 2) {
-                return i;
+    let numberPicked = 0;  // sum of numberinf of picked of two user.
+
+    const checkWinnerAllRows = chr => {
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if(gameArray[i][j] !== chr) {
+                    break;
+                }
+                if(j === 2) {
+                    return i;
+                }
             }
         }
-    }
-    return -1; // show winner not exist in i row.
-};
+        return -1; // show winner not exist in i row.
+    };
 
-const checkWinnerAllCols = chr => {
-    for(let j = 0; j < 3; j++) {
+    const checkWinnerAllCols = chr => {
+        for(let j = 0; j < 3; j++) {
+            for(let i = 0; i < 3; i++) {
+                if(gameArray[i][j] !== chr) {
+                    break;
+                }
+                if(i === 2) {
+                    return j;
+                }
+            }
+        }
+        return -1;  // show winner not exist in i column.
+    };
+
+    const checkWinnerAllDiagnal = chr => {
+        let numDiagnol = 0;
+        let numAntiDiagnol = 0;
+
         for(let i = 0; i < 3; i++) {
-            if(gameArray[i][j] !== chr) {
+            if(gameArray[i][i] !== chr) {
                 break;
             }
-            if(i === 2) {
-                return j;
+            numDiagnol++;
+        }
+
+        for(let i = 2; i >= 0; i--) {
+            if(gameArray[2 - i][i] !== chr) {
+                break;
+            }
+            numAntiDiagnol++;
+        }
+
+        if(numDiagnol === 3) {
+            return 'Diagonal';
+        } else if(numAntiDiagnol === 3) {
+            return 'AntiDiagnol';
+        } else {
+            return -1;
+        }
+    };
+
+    // handle modal for winner modal show to user.
+
+    const modalWinner = document.querySelector('.modal-winner');
+    const backdrop = document.querySelector('.backdrop');
+    const quitModalBtn = modalWinner.querySelector('button:nth-of-type(1)');
+    const nextRoundModalBtn = modalWinner.querySelector('button:nth-of-type(2)');
+
+    // button quit and next-round of modal is handled in below
+    const showWinnerModal = markType => {
+        const markWinnerModal = modalWinner.querySelector('p span');
+        if(markType === 'x') {
+            markWinnerModal.innerHTML = '<i class="fa fa-close"></i>';
+        } else if(markType === 'o') {
+            markWinnerModal.innerHTML = '<i class="fa fa-dot-circle-o"></i>';
+        } else {
+            modalWinner.querySelector('p').innerHTML = 'The game equalised'
+        }
+
+        modalWinner.style.display = 'block';
+        backdrop.style.display = 'block';
+    };
+
+    const quitModalBtnHandler = () => {
+        document.location.reload(true);
+    };
+
+    const nextRoundModalBtnHandler = () => {
+        gameArray = gameArray.map((elm) => {
+            return ['-', '-', '-'];
+        });
+
+        numberPicked = 0;
+
+        modalWinner.style.display = 'none';
+        backdrop.style.display = 'none';
+
+        for (const box of gameBoxs) {
+            box.innerHTML = '';
+            box.classList.replace('game-box-item-winned', 'game-box-item');
+            box.addEventListener('click', boxClickedHandler);
+        }
+    };
+
+    quitModalBtn.addEventListener('click', quitModalBtnHandler);
+    nextRoundModalBtn.addEventListener('click', nextRoundModalBtnHandler);
+
+    const updateScoreWinner = markType => {
+        const scoreGameMarkType = document.querySelectorAll('.score-game button');
+        if(markType === 'x') {
+            +scoreGameMarkType[0].querySelector('p').innerHTML++;
+        } else if(markType === 'o') {
+            +scoreGameMarkType[2].querySelector('p').innerHTML++;
+        } else {
+            +scoreGameMarkType[1].querySelector('p').innerHTML++;
+        }
+    };
+
+    const highliteWinnerUi = (whereWin) => {
+        if(whereWin === 'Diagonal') {
+            for(let i = 0; i < 3; i++) {
+                document.getElementById(`${i}${i}`).classList.replace('game-box-item','game-box-item-winned');
+            }
+        } else if(whereWin === 'AntiDiagnol') {
+            for(let i = 2; i >= 0; i--) {
+                document.getElementById(`${2 - i}${i}`).classList.replace('game-box-item','game-box-item-winned');
+            }
+        } else {
+            let whereWinArr = whereWin.split(' ');
+            let whereWinNumber = whereWinArr[1];
+            if(whereWinArr.includes('row')) {
+                for(let i = 0; i < 3; i++) {
+                    document.getElementById(`${whereWinNumber}${i}`).classList.replace('game-box-item','game-box-item-winned');
+                }
+            } else if(whereWinArr.includes('col')) {
+                for(let i = 0; i < 3; i++) {
+                    document.getElementById(`${i}${whereWinNumber}`).classList.replace('game-box-item','game-box-item-winned');
+                }
             }
         }
-    }
-    return -1;  // show winner not exist in i column.
-};
+    };
 
-const checkWinnerAllDiagnal = chr => {
-    let numDiagnol = 0;
-    let numAntiDiagnol = 0;
-
-    for(let i = 0; i < 3; i++) {
-        if(gameArray[i][i] !== chr) {
-            break;
+    const pickWinnerUser = (markType) => {
+        if(numberPicked < 5) {
+            return;
         }
-        numDiagnol++;
-    }
-    
-    for(let i = 2; i >= 0; i--) {
-        if(gameArray[2 - i][i] !== chr) {
-            break;
+        console.log(numberPicked);
+        let rowWinner = checkWinnerAllRows(markType);  // attention that row start from 0
+        let colWinner = checkWinnerAllCols(markType);  // attention that col start from 0
+        let diagnalWin = checkWinnerAllDiagnal(markType);
+
+        if(rowWinner !== -1) {
+            highliteWinnerUi(`row ${rowWinner}`);
+            updateScoreWinner(markType);
+            showWinnerModal(markType);
+        } else if(colWinner !== -1) {
+            highliteWinnerUi(`col ${colWinner}`);
+            updateScoreWinner(markType);
+            showWinnerModal(markType);
+        } else if(diagnalWin !== -1) {
+            highliteWinnerUi(diagnalWin);
+            updateScoreWinner(markType);
+            showWinnerModal(markType);
+        } else {
+            if(numberPicked === 9) {
+                updateScoreWinner();
+                showWinnerModal();
+            }
         }
-        numAntiDiagnol++;
-    }
+    };
 
-    if(numDiagnol === 3) {
-        return 'Diagonal';
-    } else if(numAntiDiagnol === 3) {
-        return 'AntiDiagnol';
-    } else {
-        return -1;
-    }
-};
+    const boxClickedHandler = event => {
+        console.log('clicke');
+        numberPicked++;
+        let markType = activeUser ? 'x' : 'o';
 
+        // Update active user btn in header app
+        if (activeUser && user1) {
+            activeUserBtn.innerHTML = '<i class="fa fa-dot-circle-o"></i> Turn';
+            event.target.innerHTML = '<i class="fa fa-close"></i>';
+        } else if(!activeUser, user1) {
+            activeUserBtn.innerHTML = '<i class="fa fa-close"></i> Turn';
+            event.target.innerHTML = '<i class="fa fa-dot-circle-o"></i>';
+        }
+        activeUser = !activeUser;
 
-// handle modal for winner modal show to user.
+        // create gameArray and update it with x and o
+        let btnClickedId = event.target.id;
+        let rowId = btnClickedId[0];
+        let colId = btnClickedId[1];
+        gameArray[rowId][colId] = markType;
 
-const modalWinner = document.querySelector('.modal-winner');
-const backdrop = document.querySelector('.backdrop');
-const quitModalBtn = modalWinner.querySelector('button:nth-of-type(1)');
-const nextRoundModalBtn = modalWinner.querySelector('button:nth-of-type(2)');
+        // run below function for recogonize winner
+        pickWinnerUser(markType);
 
-// button quit and next-round of modal is handled in below
-const showWinnerModal = markType => {
-    const markWinnerModal = modalWinner.querySelector('p span');
-    if(markType === 'x') {
-        markWinnerModal.innerHTML = '<i class="fa fa-close"></i>';
-    } else if(markType === 'o') {
-        markWinnerModal.innerHTML = '<i class="fa fa-dot-circle-o"></i>';
-    } else {
-        modalWinner.querySelector('p').innerHTML = 'The game equalised'
-    }
-    
-    modalWinner.style.display = 'block';
-    backdrop.style.display = 'block';
-};
-
-const quitModalBtnHandler = () => {
-    history.back();
-};
-
-const nextRoundModalBtnHandler = () => {
-    gameArray = gameArray.map((elm) => {
-        return ['-', '-', '-'];
-    });
-
-    numberPicked = 0;
-
-    modalWinner.style.display = 'none';
-    backdrop.style.display = 'none';
+        // event is deleted after that it is one time occured.
+        event.currentTarget.removeEventListener('click', boxClickedHandler);
+    };
 
     for (const box of gameBoxs) {
-        box.innerHTML = '';
-        box.classList.replace('game-box-item-winned', 'game-box-item');
         box.addEventListener('click', boxClickedHandler);
     }
-};
 
-quitModalBtn.addEventListener('click', quitModalBtnHandler);
-nextRoundModalBtn.addEventListener('click', nextRoundModalBtnHandler);
-
-const updateScoreWinner = markType => {
-    const scoreGameMarkType = document.querySelectorAll('.score-game button');
-    if(markType === 'x') {
-        +scoreGameMarkType[0].querySelector('p').innerHTML++;
-    } else if(markType === 'o') {
-        +scoreGameMarkType[2].querySelector('p').innerHTML++;
-    } else {
-        +scoreGameMarkType[1].querySelector('p').innerHTML++;
-    }
-};
-
-const highliteWinnerUi = (whereWin) => {
-    if(whereWin === 'Diagonal') {
-        for(let i = 0; i < 3; i++) {
-            document.getElementById(`${i}${i}`).classList.replace('game-box-item','game-box-item-winned');
-        }
-    } else if(whereWin === 'AntiDiagnol') {
-        for(let i = 2; i >= 0; i--) {
-            document.getElementById(`${2 - i}${i}`).classList.replace('game-box-item','game-box-item-winned');
-        }
-    } else {
-        let whereWinArr = whereWin.split(' ');
-        let whereWinNumber = whereWinArr[1];
-        if(whereWinArr.includes('row')) {
-            for(let i = 0; i < 3; i++) {
-                document.getElementById(`${whereWinNumber}${i}`).classList.replace('game-box-item','game-box-item-winned');
-            }
-        } else if(whereWinArr.includes('col')) {
-            for(let i = 0; i < 3; i++) {
-                document.getElementById(`${i}${whereWinNumber}`).classList.replace('game-box-item','game-box-item-winned');
-            }
-        }
-    }
-};
-
-const pickWinnerUser = (markType) => {
-    if(numberPicked < 5) {
-        return;
-    }
-    console.log(numberPicked);
-    let rowWinner = checkWinnerAllRows(markType);  // attention that row start from 0
-    let colWinner = checkWinnerAllCols(markType);  // attention that col start from 0
-    let diagnalWin = checkWinnerAllDiagnal(markType);
-    
-    if(rowWinner !== -1) {
-        highliteWinnerUi(`row ${rowWinner}`);
-        updateScoreWinner(markType);
-        showWinnerModal(markType);
-    } else if(colWinner !== -1) {
-        highliteWinnerUi(`col ${colWinner}`);
-        updateScoreWinner(markType);
-        showWinnerModal(markType);
-    } else if(diagnalWin !== -1) {
-        highliteWinnerUi(diagnalWin);
-        updateScoreWinner(markType);
-        showWinnerModal(markType);
-    } else {
-        if(numberPicked === 9) {
-            updateScoreWinner();
-            showWinnerModal();
-        }
-    }
-};
-
-const boxClickedHandler = event => {
-    console.log('clicke');
-    numberPicked++;
-    let markType = activeUser ? 'x' : 'o';
-
-    // Update active user btn in header app
-    if (activeUser && user1) {
-        activeUserBtn.innerHTML = '<i class="fa fa-dot-circle-o"></i> Turn';
-        event.target.innerHTML = '<i class="fa fa-close"></i>';
-    } else if(!activeUser, user1) {
-        activeUserBtn.innerHTML = '<i class="fa fa-close"></i> Turn';
-        event.target.innerHTML = '<i class="fa fa-dot-circle-o"></i>';
-    }
-    activeUser = !activeUser;
-
-    // create gameArray and update it with x and o
-    let btnClickedId = event.target.id;
-    let rowId = btnClickedId[0];
-    let colId = btnClickedId[1];
-    gameArray[rowId][colId] = markType;
-
-    // run below function for recogonize winner
-    pickWinnerUser(markType);
-
-    // event is deleted after that it is one time occured.
-    event.currentTarget.removeEventListener('click', boxClickedHandler);
-};
-
-for (const box of gameBoxs) {
-    box.addEventListener('click', boxClickedHandler);
 }
